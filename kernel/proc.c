50e6750d10e53f41c10d43397eda8666c036c3ff
Timothy Wigginton <timothywigginton1@gmail.com>
2021-10-10 10:35:46 -0400

    changed num of starting tickets

--- kernel/proc.c
+++ kernel/proc.c
@@ -64,8 +64,8 @@ allocproc(void)
 found:
   p->state = EMBRYO;
   p->pid = nextpid++;
-  p->numTicks = 10;
-  p->numTickets = 0;
+  p->numTicks = 0;
+  p->numTickets = 10;
   release(&ptable.lock);
 
   // Allocate kernel stack if possible.
